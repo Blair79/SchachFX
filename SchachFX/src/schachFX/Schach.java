7ea12616a7ff5418ad6a49501c63661b018bc8fc
Blair79 <Blair79@users.noreply.github.com>
2017-02-09 08:41:53 +0100

    Kommentare

--- SchachFX/src/schachFX/Schach.java
+++ SchachFX/src/schachFX/Schach.java
@@ -9,31 +9,31 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class Schach extends Application {
-	
+
 	Scene scene;
 
 	public static void main(String[] args) {
-		Application.launch(Schach.class, args);		
+		Application.launch(Schach.class, args);
 	}
-	
+
 	@Override
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("Schachbrett.fxml"));
-        scene = new Scene(root);
-        
-        //hinzufügen des Eventfilters um alle Möglichkeiten des DragDetect Eventhandlers zu nutzen
-        scene.addEventFilter(MouseEvent.DRAG_DETECTED , new EventHandler<MouseEvent>() {
-    	    @Override
-    	    public void handle(MouseEvent mouseEvent) {
-    	        scene.startFullDrag();
-    	    }
-    	});
-        
-        stage.setTitle("Schach");
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
+		scene = new Scene(root);
+
+		// Hinzufügen des Eventfilters, um alle Möglichkeiten des DragDetect
+		// Eventhandlers zu nutzen.
+		scene.addEventFilter(MouseEvent.DRAG_DETECTED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				scene.startFullDrag();
+			}
+		});
+
+		stage.setTitle("Schach");
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.show();
 	}
-	
-	
+
 }
--- SchachFX/src/schachFX/Schachbrett_controller.java
+++ SchachFX/src/schachFX/Schachbrett_controller.java
@@ -17,11 +17,11 @@ import javafx.scene.layout.Pane;
 
 public class Schachbrett_controller {
 
-	// FXML Variablen
-	// für die Textausgabe auf der Textarea
+	// FXML Variablen:
+	// Für die Textausgabe auf der Textarea.
 	@FXML
 	private TextArea actiontarget;
-	// für die Textausgabe auf das jeweilige Label
+	// Für die Textausgabe auf das jeweilige Label.
 	@FXML
 	private Label startposition;
 	@FXML
@@ -29,61 +29,62 @@ public class Schachbrett_controller {
 	@FXML
 	private Label endposition;
 
-	// Private Variablen des Controllers
-	// die Figur mit der etwas gemacht werden soll
+	// Private Variablen des Controllers:
+	// Die Figur mit der etwas gemacht werden soll.
 	private ImageView figur = new ImageView();
 
-	// ist der Bewegungsvorgang gestartet worden?
+	// Ist der Bewegungsvorgang gestartet worden?
 	private boolean bewegeFigur = false;
-	// das Feld aus dem die Figur gelöscht werden soll
+	// Das Feld aus dem die Figur gelöscht werden soll.
 	private Pane quellfeld = new Pane();
-	// das Feld über dem sich der Bewegungsvorgang gerade befindet
+	// Das Feld über dem sich der Bewegungsvorgang gerade befindet.
 	private Pane feld = new Pane();
-	// das Feld in das die Figur eingefügt werden soll
+	// Das Feld in das die Figur eingefügt werden soll.
 	private Pane zielfeld = new Pane();
 
-	// der Punkt von dem aus der Bewegungsvorgang gestartet wurde
+	// Der Punkt von dem aus der Bewegungsvorgang gestartet wurde.
 	private Point2D startpunkt = new Point2D(0, 0);
-	// der Punkt über dem sich der Bewegungsvorgang gerade befindet
+	// Der Punkt über dem sich der Bewegungsvorgang gerade befindet.
 	private Point2D aktuelleposition = new Point2D(0, 0);
-	// der Punkt an dem der Bewegungsvorgang beendet wurde
+	// Der Punkt an dem der Bewegungsvorgang beendet wurde.
 	private Point2D endpunkt = new Point2D(0, 0);
 
-	// FXML Funktionen
-	// eine Figur bemerkt das sie bewegt werden soll
+	// FXML Funktionen:
+	// Eine Figur bemerkt, das sie bewegt werden soll.
 	@FXML
 	protected void onDragDetected(MouseEvent event) {
 
-		// sicherstellen das die Quelle des ausgelösten Events von einer Figur
-		// stammt um gültigen Typecast durchzuführen
+		// Sicherstellen das die Quelle des ausgelösten Events von einer Figur
+		// stammt, um gültigen Typecast durchzuführen.
 		if (event.getSource() instanceof ImageView) {
 
-			// festhalten welche Figur bewegt werden soll
+			// Festhalten welche Figur bewegt werden soll.
 			figur = (ImageView) event.getSource();
 
-			// sicherstellen das die Quelle des ausgelösten Events von einem
-			// Feld stammt um gültigen Typecast durchzuführen
+			// Sicherstellen das die Quelle des ausgelösten Events von einem
+			// Feld stammt, um gültigen Typecast durchzuführen.
 			if (figur.getParent() instanceof Pane)
-				// festhalten von welchem Feld die Figur bewegt werden soll
+				// Festhalten von welchem Feld die Figur bewegt werden soll.
 				quellfeld = (Pane) (figur.getParent());
 
-			// die Figur mit einem Schlagschatten markieren
+			// Die Figur mit einem Schlagschatten markieren.
 			figur.setEffect(new DropShadow());
 
-			// Speichern der Bilddatei der Figur im ClipboardContent um bewegt
-			// anzuzeigen
+			// Speichern der Bilddatei der Figur im ClipboardContent, um deren
+			// Bewegung
+			// anzuzeigen.
 			Dragboard dragBoard = ((Node) figur).startDragAndDrop(TransferMode.ANY);
 			ClipboardContent content = new ClipboardContent();
 			content.putImage(figur.getImage());
 			dragBoard.setContent(content);
 
-			// Anzeige des Startpunktes der Bewegung zu Debug zwecken
+			// Anzeige des Startpunktes der Bewegung zu Debug Zwecken.
 			startpunkt = new Point2D(event.getSceneX(), event.getSceneY());
 			actiontarget.appendText("pressed; ");
 			String hilfsstring = startpunkt.toString();
 			startposition.setText("StartPunkt" + hilfsstring.substring(hilfsstring.lastIndexOf('D') + 1));
 
-			// der Bewegungsvorgang ist gestartet worden
+			// Der Bewegungsvorgang ist gestartet worden.
 			bewegeFigur = true;
 
 		}
@@ -91,33 +92,33 @@ public class Schachbrett_controller {
 		// event.consume(); ??
 	}
 
-	// ein Feld bemerkt das eine Figur das Feld betritt
+	// Ein Feld bemerkt, das eine Figur das Feld betritt.
 	@FXML
 	protected void onDragEntered(DragEvent event) {
 
-		// sicherstellen ob das Event durch eine Figurbewegung ausgelöst wurde
+		// Sicherstellen ob das Event durch eine Figurbewegung ausgelöst wurde.
 		if (bewegeFigur) {
 
-			// sicherstellen das die Quelle des ausgelösten Events von einem
-			// Feld stammt um gültigen Typecast durchzuführen
+			// Sicherstellen das die Quelle des ausgelösten Events von einem
+			// Feld stammt, um gültigen Typecast durchzuführen.
 			if (event.getSource() instanceof Pane)
-				// feststellen welches Feld für das auslösen des Events betreten
-				// wurde
+				// Feststellen welches Feld für das auslösen des Events betreten
+				// wurde.
 				feld = (Pane) event.getSource();
 
-			// das Feld mit einem InnerenSchatten markieren
+			// Das Feld mit einem InnerenSchatten markieren.
 			feld.setEffect(new InnerShadow());
 
 			// event.acceptTransferModes(TransferMode.MOVE); ??
 
 			// Anzeige das ein Wechsel des Feldes beim betreten und verlassen
-			// erfolgt
+			// erfolgt.
 			actiontarget.appendText("moved;");
 		}
 		// event.consume(); ??
 	}
 
-	// ein Feld bemerkt das eine Figur das Feld verlässt
+	// Ein Feld bemerkt, das eine Figur das Feld verlässt.
 	@FXML
 	protected void onDragExited(DragEvent event) {
 
@@ -142,17 +143,17 @@ public class Schachbrett_controller {
 		// event.consume(); ??
 	}
 
-	// ein Feld bemerkt das eine Figur das Feld überquert
+	// Ein Feld bemerkt das eine Figur das Feld überquert.
 	@FXML
 	protected void onDragOver(DragEvent event) {
 
-		// sicherstellen ob das Event durch eine Figurbewegung ausgelöst wurde
+		// Sicherstellen ob das Event durch eine Figurbewegung ausgelöst wurde.
 		if (bewegeFigur) {
 
-			// festlegen ob die Figur bewegt werden soll oder kopiert
+			// Festlegen ob die Figur bewegt werden soll oder kopiert.
 			event.acceptTransferModes(TransferMode.MOVE);
 
-			// Anzeige des aktuellen Position der Bewegung zu Debug zwecken
+			// Anzeige des aktuellen Position der Bewegung zu Debug Zwecken.
 			aktuelleposition = new Point2D(event.getSceneX(), event.getSceneY());
 			String hilfsstring = aktuelleposition.toString();
 			position.setText("Position" + hilfsstring.substring(hilfsstring.lastIndexOf('D') + 1));
@@ -160,32 +161,32 @@ public class Schachbrett_controller {
 		// event.consume(); ??
 	}
 
-	// ein Feld bemerkt das eine Figur auf dem Feld fallen gelassen wird
+	// Eein Feld bemerkt, das eine Figur auf dem Feld fallen gelassen wird.
 	@FXML
 	protected void onDragDropped(DragEvent event) {
 
-		// sicherstellen ob das Event durch eine Figurbewegung ausgelöst wurde
+		// Sicherstellen ob das Event durch eine Figurbewegung ausgelöst wurde.
 		if (bewegeFigur) {
-			// sicherstellen das die Quelle des ausgelösten Events von einer
-			// Figur stammt um gültigen Typecast durchzuführen
+			// Sicherstellen das die Quelle des ausgelösten Events von einer
+			// Figur stammt, um gültigen Typecast durchzuführen.
 			if ((ImageView) event.getGestureSource() instanceof ImageView)
 				quellfeld = (Pane) ((ImageView) event.getGestureSource()).getParent();
 
-			// sicherstellen das die Quelle des ausgelösten Events von einer
-			// Figur stammt um gültigen Typecast durchzuführen
+			// Sicherstellen das die Quelle des ausgelösten Events von einer
+			// Figur stammt, um gültigen Typecast durchzuführen.
 			if ((ImageView) event.getGestureSource() instanceof ImageView)
 				zielfeld = (Pane) event.getGestureTarget();
 
 			// Anzeige des von Quell- und Zielfeld auf der Konsole zu Debug
-			// zwecken
+			// Zwecken.
 			System.out.println(quellfeld + " drop " + zielfeld);
 
-			// sollten Quellfeld und Zielfeld ungleich sein die Figur dem
-			// Zielfeld hinzufügen
+			// Sollten Quellfeld und Zielfeld ungleich sein, die Figur dem
+			// Zielfeld hinzufügen.
 			if ((quellfeld != zielfeld))
 				zielfeld.getChildren().add(figur);
 
-			// Anzeige des Endposition der Bewegung zu Debug zwecken
+			// Anzeige des Endposition der Bewegung zu Debug Zwecken.
 			endpunkt = new Point2D(event.getSceneX(), event.getSceneY());
 			actiontarget.appendText("released\n");
 			String hilfsstring = endpunkt.toString();
@@ -195,36 +196,36 @@ public class Schachbrett_controller {
 		// event.consume(); ??
 	}
 
-	// abschliessende Arbeiten nach Bewegung der Figur ins Zielfeld
+	// Abschließende Arbeiten nach Bewegung der Figur ins Zielfeld.
 	@FXML
 	protected void onDragDone(DragEvent event) {
 
-		// sicherstellen ob das Event durch eine Figurbewegung ausgelöst wurde
+		// Sicherstellen ob das Event durch eine Figurbewegung ausgelöst wurde.
 		if (bewegeFigur) {
 
-			// Gleichsetzung von Zielfeld und Quellfeld um bei einer fehlenden
+			// Gleichsetzung von Zielfeld und Quellfeld, um bei einer fehlenden
 			// Zuweisung (ausgelöst durch ein verlassen des Mauscursors aus dem
 			// gültigen Schachbrett) die Ausführung der folgenden IF-Abfrage zu
-			// verhindern
+			// verhindern.
 			zielfeld = quellfeld;
-			
+
 			// Anzeige des von Quell- und Zielfeld auf der Konsole zu Debug
-			// zwecken
+			// Zwecken.
 			System.out.println(quellfeld + " done " + zielfeld);
-			
-			// sollten Quellfeld und Zielfeld ungleich sein die Figur aus dem
-			// Quellfeld entfernen
+
+			// Sollten Quellfeld und Zielfeld ungleich sein, die Figur aus dem
+			// Quellfeld entfernen.
 			if ((quellfeld != zielfeld))
 				quellfeld.getChildren().remove(figur);
-			
-			// sollte das Startfeld mit einem InnerenSchatten markiert sein
-			// diese Markierung entfernen
+
+			// Sollte das Startfeld mit einem InnerenSchatten markiert sein,
+			// diese Markierung entfernen.
 			if (quellfeld.getEffect() != null)
 				quellfeld.setEffect(null);
 
-			// der Schlagschatten der Figur wird entfernt
+			// Der Schlagschatten der Figur wird entfernt.
 			figur.setEffect(null);
-			// Reinitialisierung aller Klassenvariablen
+			// Reinitialisierung aller Klassenvariablen.
 			figur = new ImageView();
 			quellfeld = new Pane();
 			feld = new Pane();
