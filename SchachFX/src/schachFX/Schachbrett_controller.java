204bd6b0a35552560ebc8dafe5bc1854d71ff5bf
Blair79 <Blair79@users.noreply.github.com>
2017-02-12 13:29:23 +0100

    Regeln

--- SchachFX/src/schachFX/Schachbrett_controller.java
+++ SchachFX/src/schachFX/Schachbrett_controller.java
@@ -18,13 +18,12 @@ import schachFX.Spiel;
 
 public class Schachbrett_controller {
 
-	private Schach schach;
-	private Spiel spiel = new Spiel(this);
-
 	// FXML Variablen:
 	// Für die Textausgabe auf der Textarea.
 	@FXML
-	private TextArea actiontarget;
+	private TextArea statusAusgabe;
+	@FXML
+	private TextArea notation;
 	// Für die Textausgabe auf das jeweilige Label.
 	@FXML
 	private Label startposition;
@@ -33,7 +32,152 @@ public class Schachbrett_controller {
 	@FXML
 	private Label endposition;
 
+	// Felder
+	@FXML
+	private Pane a1;
+	@FXML
+	private Pane b1;
+	@FXML
+	private Pane c1;
+	@FXML
+	private Pane d1;
+	@FXML
+	private Pane e1;
+	@FXML
+	private Pane f1;
+	@FXML
+	private Pane g1;
+	@FXML
+	private Pane h1;
+
+	@FXML
+	private Pane a2;
+	@FXML
+	private Pane b2;
+	@FXML
+	private Pane c2;
+	@FXML
+	private Pane d2;
+	@FXML
+	private Pane e2;
+	@FXML
+	private Pane f2;
+	@FXML
+	private Pane g2;
+	@FXML
+	private Pane h2;
+
+	@FXML
+	private Pane a3;
+	@FXML
+	private Pane b3;
+	@FXML
+	private Pane c3;
+	@FXML
+	private Pane d3;
+	@FXML
+	private Pane e3;
+	@FXML
+	private Pane f3;
+	@FXML
+	private Pane g3;
+	@FXML
+	private Pane h3;
+
+	@FXML
+	private Pane a4;
+	@FXML
+	private Pane b4;
+	@FXML
+	private Pane c4;
+	@FXML
+	private Pane d4;
+	@FXML
+	private Pane e4;
+	@FXML
+	private Pane f4;
+	@FXML
+	private Pane g4;
+	@FXML
+	private Pane h4;
+
+	@FXML
+	private Pane a5;
+	@FXML
+	private Pane b5;
+	@FXML
+	private Pane c5;
+	@FXML
+	private Pane d5;
+	@FXML
+	private Pane e5;
+	@FXML
+	private Pane f5;
+	@FXML
+	private Pane g5;
+	@FXML
+	private Pane h5;
+
+	@FXML
+	private Pane a6;
+	@FXML
+	private Pane b6;
+	@FXML
+	private Pane c6;
+	@FXML
+	private Pane d6;
+	@FXML
+	private Pane e6;
+	@FXML
+	private Pane f6;
+	@FXML
+	private Pane g6;
+	@FXML
+	private Pane h6;
+
+	@FXML
+	private Pane a7;
+	@FXML
+	private Pane b7;
+	@FXML
+	private Pane c7;
+	@FXML
+	private Pane d7;
+	@FXML
+	private Pane e7;
+	@FXML
+	private Pane f7;
+	@FXML
+	private Pane g7;
+	@FXML
+	private Pane h7;
+
+	@FXML
+	private Pane a8;
+	@FXML
+	private Pane b8;
+	@FXML
+	private Pane c8;
+	@FXML
+	private Pane d8;
+	@FXML
+	private Pane e8;
+	@FXML
+	private Pane f8;
+	@FXML
+	private Pane g8;
+	@FXML
+	private Pane h8;
+
 	// Private Variablen des Controllers:
+
+	// Das Hauptfenster
+	private Schach schach;
+	// Das laufende Spiel
+	private Spiel spiel = new Spiel(this);
+	// Der wievielte Zug
+	int ZuegeAnzahl = 0;
+
 	// Die Figur mit der etwas gemacht werden soll.
 	private ImageView figur = new ImageView();
 
@@ -56,6 +200,249 @@ public class Schachbrett_controller {
 
 	public void setMain(Schach schach) {
 		this.schach = schach;
+	}
+
+	public void neuesSpiel() {
+
+		schach.mainWindow();
+		ausgabe("neues Spiel");
+	}
+
+	public void ausgabe(String ausgabeText) {
+
+		statusAusgabe.appendText(ausgabeText);
+	}
+
+	public void notiere(String quelle, String ziel, String notationsText) {
+
+		notation.appendText(++ZuegeAnzahl + ". " + notationsText + "\n");
+		spiel.notiere(quelle, ziel);
+
+	}
+
+	public void ziehe(String quelle, String ziel) {
+		System.out.println("ich ziehe " + quelle + " " + ziel);
+		// zielfeld.getChildren().add(figur);
+		// quellfeld.getChildren().remove(figur);
+
+		quellfeld = feldsuche(quelle);
+		if (quellfeld.getChildren().get(0) instanceof ImageView) {
+			figur = (ImageView) quellfeld.getChildren().get(0);
+		}
+		zielfeld = feldsuche(ziel);
+		zielfeld.getChildren().add(figur);
+
+		String hilfsstring = figur.getId();
+		notiere(quellfeld.getId(), zielfeld.getId(), "Ziehe " + hilfsstring.substring(0, hilfsstring.indexOf('_'))
+				+ " von " + quellfeld.getId() + " nach " + zielfeld.getId());
+	}
+
+	public Pane feldsuche(String feld) {
+		Pane gesuchtesFeld = null;
+		switch (feld) {
+		case "A1":
+			gesuchtesFeld = a1;
+			break;
+		case "A2":
+			gesuchtesFeld = a2;
+			break;
+		case "A3":
+			gesuchtesFeld = a3;
+			break;
+		case "A4":
+			gesuchtesFeld = a4;
+			break;
+		case "A5":
+			gesuchtesFeld = a5;
+			break;
+		case "A6":
+			gesuchtesFeld = a6;
+			break;
+		case "A7":
+			gesuchtesFeld = a7;
+			break;
+		case "A8":
+			gesuchtesFeld = a8;
+			break;
+
+		case "B1":
+			gesuchtesFeld = b1;
+			break;
+		case "B2":
+			gesuchtesFeld = b2;
+			break;
+		case "B3":
+			gesuchtesFeld = b3;
+			break;
+		case "B4":
+			gesuchtesFeld = b4;
+			break;
+		case "B5":
+			gesuchtesFeld = b5;
+			break;
+		case "B6":
+			gesuchtesFeld = b6;
+			break;
+		case "B7":
+			gesuchtesFeld = b7;
+			break;
+		case "B8":
+			gesuchtesFeld = b8;
+			break;
+
+		case "C1":
+			gesuchtesFeld = c1;
+			break;
+		case "C2":
+			gesuchtesFeld = c2;
+			break;
+		case "C3":
+			gesuchtesFeld = c3;
+			break;
+		case "C4":
+			gesuchtesFeld = c4;
+			break;
+		case "C5":
+			gesuchtesFeld = c5;
+			break;
+		case "C6":
+			gesuchtesFeld = c6;
+			break;
+		case "C7":
+			gesuchtesFeld = c7;
+			break;
+		case "C8":
+			gesuchtesFeld = c8;
+			break;
+
+		case "D1":
+			gesuchtesFeld = d1;
+			break;
+		case "D2":
+			gesuchtesFeld = d2;
+			break;
+		case "D3":
+			gesuchtesFeld = d3;
+			break;
+		case "D4":
+			gesuchtesFeld = d4;
+			break;
+		case "D5":
+			gesuchtesFeld = d5;
+			break;
+		case "D6":
+			gesuchtesFeld = d6;
+			break;
+		case "D7":
+			gesuchtesFeld = d7;
+			break;
+		case "D8":
+			gesuchtesFeld = d8;
+			break;
+
+		case "E1":
+			gesuchtesFeld = e1;
+			break;
+		case "E2":
+			gesuchtesFeld = e2;
+			break;
+		case "E3":
+			gesuchtesFeld = e3;
+			break;
+		case "E4":
+			gesuchtesFeld = e4;
+			break;
+		case "E5":
+			gesuchtesFeld = e5;
+			break;
+		case "E6":
+			gesuchtesFeld = e6;
+			break;
+		case "E7":
+			gesuchtesFeld = e7;
+			break;
+		case "E8":
+			gesuchtesFeld = e8;
+			break;
+
+		case "F1":
+			gesuchtesFeld = f1;
+			break;
+		case "F2":
+			gesuchtesFeld = f2;
+			break;
+		case "F3":
+			gesuchtesFeld = f3;
+			break;
+		case "F4":
+			gesuchtesFeld = f4;
+			break;
+		case "F5":
+			gesuchtesFeld = f5;
+			break;
+		case "F6":
+			gesuchtesFeld = f6;
+			break;
+		case "F7":
+			gesuchtesFeld = f7;
+			break;
+		case "F8":
+			gesuchtesFeld = f8;
+			break;
+
+		case "G1":
+			gesuchtesFeld = g1;
+			break;
+		case "G2":
+			gesuchtesFeld = g2;
+			break;
+		case "G3":
+			gesuchtesFeld = g3;
+			break;
+		case "G4":
+			gesuchtesFeld = g4;
+			break;
+		case "G5":
+			gesuchtesFeld = g5;
+			break;
+		case "G6":
+			gesuchtesFeld = g6;
+			break;
+		case "G7":
+			gesuchtesFeld = g7;
+			break;
+		case "G8":
+			gesuchtesFeld = g8;
+			break;
+
+		case "H1":
+			gesuchtesFeld = h1;
+			break;
+		case "H2":
+			gesuchtesFeld = h2;
+			break;
+		case "H3":
+			gesuchtesFeld = h3;
+			break;
+		case "H4":
+			gesuchtesFeld = h4;
+			break;
+		case "H5":
+			gesuchtesFeld = h5;
+			break;
+		case "H6":
+			gesuchtesFeld = h6;
+			break;
+		case "H7":
+			gesuchtesFeld = h7;
+			break;
+		case "H8":
+			gesuchtesFeld = h8;
+			break;
+		default:
+			System.out.println("Fehlerhafte Feldbezeichnung");
+		}
+		return gesuchtesFeld;
 	}
 
 	// FXML Funktionen:
@@ -89,7 +476,7 @@ public class Schachbrett_controller {
 
 			// Anzeige des Startpunktes der Bewegung zu Debug Zwecken.
 			startpunkt = new Point2D(event.getSceneX(), event.getSceneY());
-			actiontarget.appendText("pressed; ");
+			ausgabe("pressed; ");
 			String hilfsstring = startpunkt.toString();
 			startposition.setText("StartPunkt" + hilfsstring.substring(hilfsstring.lastIndexOf('D') + 1));
 
@@ -122,7 +509,7 @@ public class Schachbrett_controller {
 
 			// Anzeige das ein Wechsel des Feldes beim betreten und verlassen
 			// erfolgt.
-			actiontarget.appendText("moved;");
+			ausgabe("moved;");
 		}
 		// event.consume(); ??
 	}
@@ -188,16 +575,21 @@ public class Schachbrett_controller {
 
 			// Anzeige des von Quell- und Zielfeld auf der Konsole zu Debug
 			// Zwecken.
-			System.out.println(quellfeld + " drop " + zielfeld);
+			// System.out.println(quellfeld + " drop " + zielfeld);
 
 			// Sollten Quellfeld und Zielfeld ungleich sein, die Figur dem
 			// Zielfeld hinzufügen.
-			if ((quellfeld != zielfeld))
+			if ((quellfeld != zielfeld) && (spiel.istZugerlaubt(figur.getId(), quellfeld.getId(), zielfeld.getId()))) {
 				zielfeld.getChildren().add(figur);
+				String hilfsstring = figur.getId();
+				notiere(quellfeld.getId(), zielfeld.getId(),
+						"Ziehe " + hilfsstring.substring(0, hilfsstring.indexOf('_')) + " von " + quellfeld.getId()
+								+ " nach " + zielfeld.getId());
+			}
 
 			// Anzeige des Endposition der Bewegung zu Debug Zwecken.
 			endpunkt = new Point2D(event.getSceneX(), event.getSceneY());
-			actiontarget.appendText("released\n");
+			ausgabe("released\n");
 			String hilfsstring = endpunkt.toString();
 			endposition.setText("EndPunkt" + hilfsstring.substring(hilfsstring.lastIndexOf('D') + 1));
 
@@ -247,6 +639,8 @@ public class Schachbrett_controller {
 
 			// der Bewegungsvorgang ist beendet worden
 			bewegeFigur = false;
+			// TODO Computerspieler starten
+			// spiel.ziehe("E7","E6");
 		}
 
 		// event.consume(); ??
--- SchachFX/src/schachFX/Spiel.java
+++ SchachFX/src/schachFX/Spiel.java
@@ -7,4 +7,139 @@ public class Spiel {
 		this.schachbrett_controller = schachbrett_controller;
 	}
 
+	public void notiere(String quelle, String ziel) {
+		// TODO Speichern der Notation
+		// System.out.println(quelle+","+ziel);
+	}
+
+	public void ziehe(String quelle, String ziel) {
+		schachbrett_controller.ziehe(quelle, ziel);
+	}
+
+	public boolean istZugerlaubt(String figur, String quelle, String ziel) {
+
+		System.out.println(figur + " " + quelle + " " + ziel);
+
+		// true = rauf, false = runter
+		boolean richtung;
+		boolean erlaubt = false;
+
+		if (figur.contains("_w"))
+			richtung = true;
+		else
+			richtung = false;
+		System.out.println(richtung);
+
+		String ergebnis = "";
+		char hilfsPosX = quelle.charAt(0);
+		int hilfsPosY = quelle.charAt(1);
+		// TODO Spielregeln
+		switch (figur.substring(0, figur.indexOf('_'))) {
+		case "Bauer":
+
+			ergebnis = linksRauf(quelle, 1, richtung);
+			ergebnis = ergebnis + (rauf(quelle, 1, richtung));
+			if ((quelle.charAt(1) == '2') || (quelle.charAt(1) == '7'))
+				ergebnis = ergebnis + (rauf(quelle, 2, richtung));
+			ergebnis = ergebnis + rechtsRauf(hilfsPosX, hilfsPosY, 1, richtung);
+
+			erlaubt = ergebnis.contains(ziel);
+
+			// System.out.println(ergebnis);
+			break;// 3 Richtungen 4 Felder
+		case "Turm":
+			//TODO Umkehr
+			if (quelle.charAt(1)>ziel.charAt(1));
+			int j=1;
+			for (int i = quelle.charAt(1); i < ziel.charAt(1); i++) {
+				System.out.println("test");
+				ergebnis = ergebnis + rauf(quelle, j++, richtung);
+			}
+			
+			erlaubt = ergebnis.contains(ziel);
+			System.out.println(ergebnis);
+			break;// 4 Richtungen
+		case "Springer":
+			System.out.println("Springer");
+			break;// 8 Richtungen 8 Felder
+		case "Lauefer":
+			System.out.println("Lauefer");
+			break;// 4 Richtungen um 45
+		case "Dame":
+			System.out.println("Dame");
+			break;
+		case "Koenig":
+			System.out.println("Koenig");
+			break;
+
+		}
+
+		return erlaubt;
+
+	}
+
+	private String rauf(String quelle, int anzahl, boolean richtung) {
+		char hilfsPosX = quelle.charAt(0);
+		int hilfsPosY = quelle.charAt(1);
+		int i = 7;
+		if (!richtung) {
+			i = 2;
+			anzahl *= -1;
+		}
+
+		String ergebnis = "";
+		// Rauf
+		if (hilfsPosY != i) {
+			hilfsPosY += anzahl - 48;
+			ergebnis = ergebnis + hilfsPosX + hilfsPosY;
+		}
+
+		return ergebnis;
+
+	}
+
+	private String linksRauf(String quelle, int anzahl, boolean richtung) {
+		char hilfsPosX = quelle.charAt(0);
+		int hilfsPosY = quelle.charAt(1);
+		int i = 7;
+		if (!richtung) {
+			i = 2;
+			anzahl *= -1;
+		}
+
+		String ergebnis = "";
+		// Links rauf
+		if ((hilfsPosX != 'A')) {
+
+			hilfsPosX -= (char) Math.abs(anzahl);//
+			if (hilfsPosY != i)
+				hilfsPosY += anzahl - 48;
+			ergebnis = ergebnis + hilfsPosX + hilfsPosY;
+
+		}
+
+		return ergebnis;
+
+	}
+
+	private String rechtsRauf(char hilfsPosX, int hilfsPosY, int anzahl, boolean richtung) {
+
+		int i = 7;
+		if (!richtung) {
+			i = 2;
+			anzahl *= -1;
+		}
+
+		String ergebnis = "";
+		// Rechts rauf
+		if ((hilfsPosX != 'H')) {
+			hilfsPosX += (char) 1;//
+			if (hilfsPosY != i)
+				hilfsPosY += anzahl - 48;
+			ergebnis = ergebnis + hilfsPosX + hilfsPosY;
+		}
+
+		return ergebnis;
+
+	}
 }
